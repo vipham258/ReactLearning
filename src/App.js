3cffe72cb22daf0d07314a781607f2338ac9dd5c
Vi Pham <Vi.Pham@inspection.gc.ca>
2019-05-02 13:53:15 -0400

    added Flexible lists

--- src/App.js
+++ src/App.js
@@ -13,13 +13,20 @@ class App extends Component {
     showPersons: false
   };
 
-  nameChangedHandler = event => {
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = { ...this.state.persons[personIndex] };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
     this.setState({
-      persons: [
-        { name: "Max", age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: "Stephanie", age: 26 }
-      ]
+      persons: persons
     });
   };
 
@@ -54,6 +61,7 @@ class App extends Component {
                 name={person.name}
                 age={person.age}
                 key={person.id}
+                changed={event => this.nameChangedHandler(event, person.id)}
               />
             );
           })}
