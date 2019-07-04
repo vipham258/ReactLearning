31fd6bd17d0df0bc03c9b98880f8db41d79cd6ad
Vi Pham <Vi.Pham@inspection.gc.ca>
2019-07-04 15:18:56 -0400

    fixed the "wrong thing"

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -3,7 +3,7 @@ import React from "react";
 import Person from "./Person/Person";
 
 const persons = props => {
-  props.persons.map((person, index) => {
+  return props.persons.map((person, index) => {
     return (
       <Person
         click={() => props.clicked(index)}
